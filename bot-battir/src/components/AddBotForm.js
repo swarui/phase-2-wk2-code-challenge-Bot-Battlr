f842f30703db1571c2b9aa75c7146736341fbc5f
swarui <stevewaruim@gmail.com>
2023-11-06 00:05:02 +0300

    Added form state management for bot creation in AddBotForm component

---
+++ bot-battir/src/components/AddBotForm.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const AddBotForm = ({ addBot }) => {
+  const [name, setName] = useState("");
+  const [health, setHealth] = useState("");
+  const [damage, setDamage] = useState("");
+  const [armor, setArmor] = useState("");
+  const [bot_class, setBotClass] = useState("");
+  const [catchphrase, setCatchphrase] = useState("");
+  const [avatar_url, setAvatarUrl] = useState("");
+};
+
