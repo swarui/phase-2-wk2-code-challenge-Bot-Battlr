e7125ed3ccea0b562c66423a21122e8703c50153
swarui <stevewaruim@gmail.com>
2023-11-05 19:02:06 +0300

    Initialize state variables and fetch data

--- bot-battir/src/App.js
+++ bot-battir/src/App.js
@@ -4,3 +4,15 @@ import MyBotArmy from './components/MyBotArmy';
 import './App.css';
 import AddBotForm from './components/AddBotForm';
 
+const App = () =>{
+  // Define the state variables using state hooks
+  const [bots, setBots] = useState([]);
+  const [myBotArmy, setMyBotArmy] = useState([]);
+
+  // Fetch data from the server
+  fetch('https://template-1-0s60.onrender.com/bots')
+  .then((response) => response.json())
+  .then((data) => setBot(data))
+  .then((error) => console.error('Error fetching data:', error));
+  
+}
