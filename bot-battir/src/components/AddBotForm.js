9df59c6ea8d7a1f28f35e49f5055af22c9d43073
swarui <stevewaruim@gmail.com>
2023-11-06 00:17:14 +0300

    Added a form component for creating new bots

--- bot-battir/src/components/AddBotForm.js
+++ bot-battir/src/components/AddBotForm.js
@@ -1,16 +1,15 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const AddBotForm = ({ addBot }) => {
-  const [name, setName] = useState("");
-  const [health, setHealth] = useState("");
-  const [damage, setDamage] = useState("");
-  const [armor, setArmor] = useState("");
-  const [bot_class, setBotClass] = useState("");
-  const [catchphrase, setCatchphrase] = useState("");
-  const [avatar_url, setAvatarUrl] = useState("");
-};
+  const [name, setName] = useState('');
+  const [health, setHealth] = useState('');
+  const [damage, setDamage] = useState('');
+  const [armor, setArmor] = useState('');
+  const [bot_class, setBotClass] = useState('');
+  const [catchphrase, setCatchphrase] = useState('');
+  const [avatar_url, setAvatarUrl] = useState('');
 
-const handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     const newBot = {
       name,
