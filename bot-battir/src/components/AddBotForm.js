94c370360461500e4d4fc0ed7ced77fb64a48b7a
swarui <stevewaruim@gmail.com>
2023-11-06 00:16:29 +0300

    Added a form component for creating new bots

--- bot-battir/src/components/AddBotForm.js
+++ bot-battir/src/components/AddBotForm.js
@@ -30,3 +30,36 @@ const handleSubmit = (event) => {
     setCatchphrase('');
     setAvatarUrl('');
   };
+
+  return (
+    <div className="add-bot-form">
+      <h2>Add a New Bot</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+
+        <label>Health:</label>
+        <input type="number" value={health} onChange={(e) => setHealth(e.target.value)} required />
+
+        <label>Damage:</label>
+        <input type="number" value={damage} onChange={(e) => setDamage(e.target.value)} required />
+
+        <label>Armor:</label>
+        <input type="number" value={armor} onChange={(e) => setArmor(e.target.value)} required />
+
+        <label>Class:</label>
+        <input type="text" value={bot_class} onChange={(e) => setBotClass(e.target.value)} required />
+
+        <label>Catchphrase:</label>
+        <input type="text" value={catchphrase} onChange={(e) => setCatchphrase(e.target.value)} required />
+
+        <label>Avatar URL:</label>
+        <input type="text" value={avatar_url} onChange={(e) => setAvatarUrl(e.target.value)} required />
+
+        <button className="button-78" type="submit">Add Bot</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddBotForm;
