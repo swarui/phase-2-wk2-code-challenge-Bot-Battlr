cb7c384e0ea0caf0589c4a15b79db1f9b025afe6
swarui <stevewaruim@gmail.com>
2023-11-06 08:51:37 +0300

    Added 'MyBotArmy' React component to render a list of bots with discharge functionality

---
+++ bot-battir/src/components/MyBotArmy.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import MyBotCard from './MyBotCard';
+
+const MyBotArmy = ({ bots, dischargeBot }) => {
+  return (
+    <div className='your-bot-army'>
+      <h2>My Bot Army</h2>
+      {bots.map((bot) => (
+        <MyBotCard
+          key={bot.id}
+          bot={bot}
+          actionLabel="Discharge"
+          actionHandler={() => dischargeBot(bot.id)}
+        />
+      ))}
+    </div>
+  )
+}
+
